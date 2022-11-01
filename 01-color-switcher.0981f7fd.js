f51c32bf6438f597d1120e40199785410d5a8638
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-11-01 01:27:46 +0000

    Deploying to gh-pages from @ MikoreVikt/goit-js-hw-09@4de0fc4231b57151f239de3c0065f687cb958747 🚀

---
+++ 01-color-switcher.0981f7fd.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("body"),n=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");n.addEventListener("click",(function(){n.disabled=!0,o.disabled=!1,t=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),o.addEventListener("click",(function(){clearInterval(t),n.disabled=!1,o.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.0981f7fd.js.map
---
+++ 01-color-switcher.865017d3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let o;e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),d.addEventListener("click",(function(){clearInterval(o),e.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.865017d3.js.map
