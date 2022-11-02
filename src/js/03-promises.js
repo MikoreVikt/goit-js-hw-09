284d4279d4103a54c65838ec86c5eb51338003ea
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-11-02 20:38:07 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,14 +6,13 @@ Notiflix.Notify.init({
 });
 
 const getEl = selector => document.querySelector(selector);
-getEl(`form`).addEventListener(`submit`, onSubmitClick);
 
-function onSubmitClick(e) {
-  e.preventDefault();
-  const el = e.target.elements;
-  let delay = Number(el.delay.value);
-  let step = Number(el.step.value);
-  const amount = Number(el.amount.value);
+getEl(`form`).addEventListener(`submit`, evt => {
+  evt.preventDefault();
+
+  let delay = Number(evt.target.elements.delay.value);
+  let step = Number(evt.target.elements.step.value);
+  let amount = Number(evt.target.elements.amount.value);
 
   for (let i = 1; i <= amount; i += 1) {
     createPromise(i, delay)
@@ -30,12 +29,13 @@ function onSubmitClick(e) {
 
     delay += step;
   }
-}
+});
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
