9687388f6e86ce17f0af4075ade241514b0fdb07
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-11-02 14:06:45 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.init({
+  position: 'right-top',
+  clickToClose: false,
+});
+
+const getEl = selector => document.querySelector(selector);
+getEl(`form`).addEventListener(`submit`, onBtnClick);
+
+function onBtnClick(e) {
+  e.preventDefault();
+  const el = e.target.elements;
+  let delay = Number(el.delay.value);
+  let step = Number(el.step.value);
+  const amount = Number(el.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
